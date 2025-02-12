49caecd7bf7a9439df639a4ea99051aec5832030
AngelStgo <euangel.stgo@gmail.com>
2025-02-11 23:34:26 -0500

    added income, expenses

--- script.js
+++ script.js
@@ -4,31 +4,58 @@ const transactions = [
         name: 'salary',
         amount: 5000,
         date: new Date(),
-        type: 'income'
+        type: 'income',
     },
     {
         id: 2,
         name: 'haircut',
         amount: 30,
         date: new Date(),
-        type: 'expense'
+        type: 'expense',
     },
     {
         id: 3,
         name: 'concert ticket',
         amount: 200,
-        date: new Date(),
-        type: 'expense'
+        date: new Date(), 
+        type: 'expense',
     },
 ];
 
+
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    signDisplay: 'always',
+})
+
+
 const list = document.getElementById("transactionList");
+const status = document.getElementById("status");
 
 function renderList () {
-    list.innerHTML = '<li>item</li>';
-    transactions.forEach((transaction) => {
+    list.innerHTML = "";
+
+    if (transactions.length === 0) {
+        status.textContent = 'No transactions.';
+        return;
+    }
+    transactions.forEach(({ id, name, amount, date, type}) => {
         const li = document.createElement("li");
-        li.innerHTML = transaction.amount;
+
+        // use backticks `
+        li.innerHTML = ` 
+            <div class="name">
+          <h4>${name}</h4>
+          <p>${new Date(date).toLocaleDateString()}</p>
+          </div>
+
+          <div class="amount ${type}">
+          <span>${formatter.format(amount)}</span>
+          </div>
+
+         `;
+        list.appendChild(li);
     })
 }
 
